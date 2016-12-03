40b3128af72611a64f9551405142fa2cf8928b77
James <jbkuczma@buffalo.edu>
2016-12-02 20:11:19 -0500

    added html file to render

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, current_app
 app = Flask(__name__)
 
 @app.route('/')
 def hey():
-	return 'hey world'
+	return current_app.send_static_file('index.html')
