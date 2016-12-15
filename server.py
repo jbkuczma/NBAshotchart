4db90a91267cd80328b1c541b678a7fcca694c68
James <jbkuczma@buffalo.edu>
2016-12-15 10:48:40 -0500

    changed port for heroku

--- server.py
+++ server.py
@@ -60,5 +60,10 @@ def player(playerID):
 
 @app.after_request
 def addHeader(response):
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
+	response.headers['Cache-Control'] = 'public, max-age=0'
+	return response
+
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 33507))
+	app.run(host='0.0.0.0', port=port)
