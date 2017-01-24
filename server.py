ef460fb217fb4d8114a0595537f1836186e350ae
James <jbkuczma@buffalo.edu>
2017-01-24 09:54:38 -0500

    hmmmm

--- server.py
+++ server.py
@@ -65,6 +65,6 @@ def addHeader(response):
 
 
 if __name__ == "__main__":
-	# port = int(os.environ.get("PORT", 33507))
-	# app.run(host='0.0.0.0', port=port)
-	app.run()
+	port = int(os.environ.get("PORT", 33507))
+	app.run(host='0.0.0.0', port=port)
+	# app.run()
